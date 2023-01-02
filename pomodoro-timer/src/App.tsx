8d06a862e15f9ba527e5db32f8fdf1315564603a
leandro <fahurleandro@gmail.com>
2023-01-02 10:34:40 -0800

    basic styled components

---
+++ pomodoro-timer/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button/Button.component";
+
+export function App() {
+  return (
+    <div>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </div>
+  );
+}
---
+++ pomodoro-timer/src/components/Button/Button.component.tsx
@@ -0,0 +1,11 @@
+import { ButtonContainer, ButtonVariant } from "./Button.styles";
+
+interface ButtonProps {
+  variant?: ButtonVariant;
+}
+
+export const Button = (props: ButtonProps) => {
+  const { variant = "primary" } = props;
+
+  return <ButtonContainer variant={variant}>Click Me!</ButtonContainer>;
+};
