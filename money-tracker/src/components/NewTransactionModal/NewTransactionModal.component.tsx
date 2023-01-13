0ab89cb23bdb3a4c4c88b50bb3ca6093231c61e0
leandro <fahurleandro@gmail.com>
2023-01-13 08:03:02 -0800

    finished the UI, now added a fake JSON server

--- money-tracker/src/components/NewTransactionModal/NewTransactionModal.component.tsx
+++ money-tracker/src/components/NewTransactionModal/NewTransactionModal.component.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { Content, Overlay } from './NewTransactionModal.styled'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './NewTransactionModal.styled'
 
 export function NewTransactionModal() {
   return (
@@ -8,16 +9,32 @@ export function NewTransactionModal() {
 
       <Content>
         <Dialog.Title>New Transaction</Dialog.Title>
+        <CloseButton>
+          <X size={20} />
+        </CloseButton>
         <form action=''>
-          <input type="text" placeholder='Description' required/>
-          <input type="number" placeholder='Amount' required/>
-          <input type="text" placeholder='Category' required/>
+          <input type="text" placeholder='Description' required />
+          <input type="number" placeholder='Amount' required />
+          <input type="text" placeholder='Category' required />
+
+          <TransactionType>
+
+            <TransactionTypeButton variant='income' value='income'>
+              <ArrowCircleUp size={24} />
+              Income
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant='outgoing' value='outgoing'>
+              <ArrowCircleDown size={24} />
+              Outgoing
+            </TransactionTypeButton>
+
+          </TransactionType>
 
           <button type='submit'>Register</button>
         </form>
-        <Dialog.Close />
       </Content>
 
-    </Dialog.Portal>
+    </Dialog.Portal >
   )
 }
